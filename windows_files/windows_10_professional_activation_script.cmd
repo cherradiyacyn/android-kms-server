8e324a6f3e7af1820709f88828215d37c94e60cf
Yacyn CHERRADI <cherradiyacyn@gmail.com>
2023-10-14 19:16:08 +0100

    rem

--- windows_files/windows_10_professional_activation_script.cmd
+++ windows_files/windows_10_professional_activation_script.cmd
@@ -1,8 +1,8 @@
 @setlocal enableextensions
 
 @set key=W269N-WFGWX-YVC9B-4J6C9-T83GX
-:: repalce 127.0.0.1 with IP
-:: in Termux; Get IP by typing ifconfig command
+@rem repalce 127.0.0.1 with IP
+@rem in Termux; Get IP by typing ifconfig command
 @set ipAddr=127.0.0.1
 @set port=1688
 
