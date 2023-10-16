4bb4742cd4604eba2dc28bd2cc8016daebc5261c
Yacyn CHERRADI <cherradiyacyn@gmail.com>
2023-10-16 02:32:53 +0100

    version 2

--- windows_files/office_16_standard_activation_script.cmd
+++ windows_files/office_16_standard_activation_script.cmd
@@ -1,17 +1,16 @@
-@setlocal enableextensions
+@echo off
+set key=JNRGM-WHDWX-FJJG3-K47QV-DRTFM
+rem repalce 127.0.0.1 with IP
+rem in Termux; Get IP by typing ifconfig command
+set ipAddr=127.0.0.1
+set port=1688
 
-@set key=JNRGM-WHDWX-FJJG3-K47QV-DRTFM
-@rem repalce 127.0.0.1 with IP
-@rem in Termux; Get IP by typing ifconfig command
-@set ipAddr=127.0.0.1
-@set port=1688
+cd "C:\Program Files\Microsoft Office\Office16"
+cscript ospp.vbs /inpkey:%key%
+cscript ospp.vbs /sethst:%ipAddr%
+cscript ospp.vbs /setprt:%port%
+cscript ospp.vbs /act
+cscript ospp.vbs /dstatusall
 
-@cd "C:\Program Files\Microsoft Office\Office16"
-@cscript ospp.vbs /inpkey:%key%
-@cscript ospp.vbs /sethst:%ipAddr%
-@cscript ospp.vbs /setprt:%port%
-@cscript ospp.vbs /act
-@cscript ospp.vbs /dstatusall
-
-@cd "C:\Users\%username%\Desktop"
-@pause
+cd "C:\Users\%username%\Desktop"
+pause
--- windows_files/windows_10_professional_activation_script.cmd
+++ windows_files/windows_10_professional_activation_script.cmd
@@ -1,16 +1,15 @@
-@setlocal enableextensions
+@echo off
+set key=W269N-WFGWX-YVC9B-4J6C9-T83GX
+rem repalce 127.0.0.1 with IP
+rem in Termux; Get IP by typing ifconfig command
+set ipAddr=127.0.0.1
+set port=1688
 
-@set key=W269N-WFGWX-YVC9B-4J6C9-T83GX
-@rem repalce 127.0.0.1 with IP
-@rem in Termux; Get IP by typing ifconfig command
-@set ipAddr=127.0.0.1
-@set port=1688
+slmgr.vbs -upk
+slmgr.vbs -ipk %key%
+slmgr.vbs -skms %ipAddr%:%port%
+slmgr.vbs -ato
+slmgr.vbs -dlv
 
-@slmgr.vbs -upk
-@slmgr.vbs -ipk %key%
-@slmgr.vbs -skms %ipAddr%:%port%
-@slmgr.vbs -ato
-@slmgr.vbs -dlv
-
-@cd "C:\Users\%username%\Desktop"
-@pause
+cd "C:\Users\%username%\Desktop"
+pause
